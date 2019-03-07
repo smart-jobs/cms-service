711f6f608193f96e93b4d33d4baa52554b4367d8
dygapp <dygapp@qq.com>
2019-03-07 17:37:42 +0800

    first commit

--- config/config.default.js
+++ config/config.default.js
@@ -4,10 +4,17 @@ module.exports = appInfo => {
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1517455121740_7922';
+  config.keys = appInfo.name + '_1517455121740_8202';
 
   // add your config here
   // config.middleware = [];
+
+  config.cluster = {
+    listen: {
+      port: 8202,
+    },
+  };
+
   config.errorMongo = {
     details: true,
   };
@@ -17,7 +24,7 @@ module.exports = appInfo => {
 
   // mongoose config
   config.mongoose = {
-    url: 'mongodb://localhost:27017/platform',
+    url: 'mongodb://localhost:27017/cms',
     options: {
       user: 'root',
       pass: 'Ziyouyanfa#@!',
--- config/config.local.js
+++ config/config.local.js
@@ -5,12 +5,13 @@ module.exports = () => {
 
   // mongoose config
   config.mongoose = {
-    url: 'mongodb://localhost:27018/cms',
+    url: 'mongodb://192.168.18.100:27018/cms',
+    // url: 'mongodb://192.168.1.170:27018/cms',
   };
 
   config.logger = {
-    // level: 'DEBUG',
-    // consoleLevel: 'DEBUG',
+    level: 'DEBUG',
+    consoleLevel: 'DEBUG',
   };
 
   return config;
